e3e6c5980bd3a81bb966183f6f6483ded48b0a53
recamelflores <148180833+recamelflores@users.noreply.github.com>
2023-10-24 13:06:18 +0800

    Add files via upload

---
+++ MIDTERM/Student.java
@@ -0,0 +1,48 @@
+  
+class Student{
+ 
+ //attributes
+ String first_name ="";
+ String middle_name ="";
+ String last_name ="";
+ String suffix="";
+
+ 
+ public Student() {
+}
+
+ //Getters
+  public String getFirstName(){
+   return first_name;
+}
+
+  public String getMiddleName(){
+   return middle_name;
+}
+
+ public String getLastName(){
+   return last_name;
+}
+
+ public String Suffix(){  
+    return suffix;
+}
+
+  //Setters
+  public void setFirstName(String firstName);
+  firstName = firstName;
+}   
+  
+  public void setMiddleName(String middleName);
+  middleName = middleName;
+}     
+
+  public void setMiddleName(String lastName);
+  lastName = lastName;
+}     
+
+  public void setSuffix(String suffix);
+  suffix = suffix;
+}     
+
+
